011f29bf688cd0da5410497569c75f950d0f2bca
Nikola Škorić <nskoric@gmail.com>
2011-08-28 01:48:52 +0200

    NodeView now works as expected

--- SimGUI/Browser.cs
+++ SimGUI/Browser.cs
@@ -4,12 +4,11 @@ using System;
 namespace SimGUI
 {
 
-	public partial class Browser : Gtk.Window
-	{
+	public partial class Browser : Gtk.Window {
 		ErlInterface simInterface;
-		
-		protected virtual void OnButton1Clicked (object sender, System.EventArgs e)
-		{
+		SimEntry selected;
+
+		protected virtual void OnButton1Clicked(object sender, System.EventArgs e) {
 			string username = this.entry3.Text;
 			string server_address = this.entry2.Text;
 			int server_port = this.spinbutton1.ValueAsInt;
@@ -22,61 +21,68 @@ namespace SimGUI
 				"Server not running on " + server_address + ":" + server_port + ".");
 				md.Run();
 				md.Destroy();
-			} catch {			
+			} catch {
 				this.simInterface = null;
 				MessageDialog md = new MessageDialog(this, DialogFlags.Modal, MessageType.Error, ButtonsType.Close,
 				"Connection error.");
 				md.Run();
 				md.Destroy();
 			}
-			if (this.simInterface != null) {
-				this.textview1.Buffer.Text = this.simInterface.listSims();
-			}
-
+			this.RefreshSimList();
+			
 		}
-		
-		
 
-		public Browser () : base(Gtk.WindowType.Toplevel)
-		{
-			this.Build ();
+
+
+		public Browser() : base(Gtk.WindowType.Toplevel) {
+			this.Build();
 			this.simInterface = null;
-						
-			//this.nodeview1.Model = new SimulatorNode("");
-			this.nodeview1.AppendColumn("Id", new Gtk.CellRendererText (), "text", 0);
-			this.nodeview1.AppendColumn("Name", new Gtk.CellRendererText (), "text", 1);
+			
+			this.selected = null;
+			
+			this.nodeview1.AppendColumn("Id", new Gtk.CellRendererText(), "text", 0);
+			this.nodeview1.AppendColumn("Name", new Gtk.CellRendererText(), "text", 1);
+			this.nodeview1.AppendColumn("Description", new Gtk.CellRendererText(), "text", 2);
+			this.nodeview1.AppendColumn("Owner", new Gtk.CellRendererText(), "text", 3);
+			this.nodeview1.NodeSelection.Changed += new System.EventHandler(OnSelectionChanged);
 			this.nodeview1.ShowAll();
+			// Create our TreeView and add it as our child widget
+			
 			
 			this.DeleteEvent += this.Disconnect;
 		}
-		protected virtual void OnButton3Clicked (object sender, System.EventArgs e)
-		{
+
+		void OnSelectionChanged(object o, System.EventArgs args) {
+			Gtk.NodeSelection selection = (Gtk.NodeSelection)o;
+			SimEntry node = (SimEntry)selection.SelectedNode;
+			if (node != null) {
+				this.selected = node;
+			}
+		}
+
+		protected virtual void OnButton3Clicked(object sender, System.EventArgs e) {
 			this.RefreshSimList();
 		}
-		
-		protected virtual void OnButton4Clicked (object sender, System.EventArgs e)
-		{
+
+		protected virtual void OnButton4Clicked(object sender, System.EventArgs e) {
 			if (this.simInterface != null) {
-				this.textview1.Buffer.Text = "";
 				this.simInterface.Disconnect();
 				this.simInterface = null;
 			} else {
-				MessageDialog md = new MessageDialog(this, DialogFlags.Modal, MessageType.Error, ButtonsType.Close,
-				"Not connected to a server.");
+				MessageDialog md = new MessageDialog(this, DialogFlags.Modal, MessageType.Error, ButtonsType.Close, "Not connected to a server.");
 				md.Run();
 				md.Destroy();
 			}
 		}
-		
-		protected virtual void OnButton2Clicked (object sender, System.EventArgs e)
-		{
+
+		protected virtual void OnButton2Clicked(object sender, System.EventArgs e) {
 			NewSimStarter nss = new NewSimStarter(this.simInterface, this);
 			nss.ShowAll();
 		}
-		
+
 		public void RefreshSimList() {
 			if (this.simInterface != null) {
-				this.textview1.Buffer.Text = this.simInterface.listSims();
+				this.nodeview1.NodeStore = this.simInterface.generateStore();
 			} else {
 				MessageDialog md = new MessageDialog(this, DialogFlags.Modal, MessageType.Error, ButtonsType.Close,
 				"Not connected to a server.");
@@ -84,23 +90,23 @@ namespace SimGUI
 				md.Destroy();
 			}
 		}
-		
+
 		public void Disconnect(object obj, DeleteEventArgs args) {
 			this.simInterface.Disconnect();
 		}
-		
+
 		~Browser() {
 			this.simInterface.Disconnect();
 		}
-		
-		protected virtual void OnButton35Clicked (object sender, System.EventArgs e)
-		{
-			string simId = this.entry1.Text;
+
+		protected virtual void OnButton35Clicked(object sender, System.EventArgs e) {
+			if (this.selected != null) {
+				this.simInterface.ConnectToSim(this.selected.SimId);
+				
+				Startup su = new Startup(this.simInterface);
+				su.ShowAll();
+			}
 			
-			this.simInterface.ConnectToSim(simId);
-			
-			Startup su = new Startup(this.simInterface);
-			su.ShowAll();
 		}
 		
 		
--- SimGUI/ErlInterface.cs
+++ SimGUI/ErlInterface.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using System.Net.Sockets;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Collections;
 
 namespace SimGUI {
 
@@ -10,49 +11,56 @@ namespace SimGUI {
 		public NetworkStream stream;
 		public String username;
 		public String server;
-		
+
+
+		public Gtk.NodeStore generateStore() {
+			Gtk.NodeStore store = new Gtk.NodeStore(typeof(SimEntry));
+			
+			foreach (SimEntry s in this.listSims()) {
+				store.AddNode(s);
+			}
+			return store;
+		}
+
 		public ErlInterface(String username, String server, int port) {
 			TcpClient client;
 			
 			this.server = server;
 			this.username = username;
-
+			
 			client = new TcpClient(server, port);
 			this.stream = client.GetStream();
 			
 		}
-		
-		public String listSims() {
+
+		public ArrayList listSims() {
 			String response = this.Call("{ask, list_sims}");
 			String pattern = @"{(.+)}";
 			
 			MatchCollection matches = Regex.Matches(response, pattern);
 			
-			String retval = "";
+			ArrayList sims = new ArrayList();
 			foreach (Match match in matches) {
-				retval += match.Groups[1].Value + "\n";
+				sims.Add(new SimEntry(match.Groups[1].Value));
 			}
 			
-			if (retval == "") {
-				return "No simulators.";
-			}
-			return String.Join(" ", retval.Split(','));
+			return sims;
 			
 		}
-		
+
 		public bool ConnectToSim(string simId) {
 			string response = this.Call("{ask, connect_to_simulator, [" + simId + ", \"" + username + "\"]}");
 			string pattern = "{connected,(.+)}";
 			
 			Match match = Regex.Matches(response, pattern)[0];
 			int port = int.Parse(match.Groups[1].Value);
-
+			
 			TcpClient client = new TcpClient(this.server, port);
 			this.stream = client.GetStream();
-
+			
 			return true;
 		}
-		
+
 		public String Call(String parameter) {
 			
 			Byte[] data = Encoding.ASCII.GetBytes(parameter);
@@ -65,13 +73,59 @@ namespace SimGUI {
 			responseData = Encoding.ASCII.GetString(data, 0, bytes);
 			return responseData;
 		}
-		
+
 		public void Disconnect() {
 			this.stream.Close();
 		}
-		
+
 		~ErlInterface() {
 			this.Disconnect();
 		}
+	}
+
+
+	[Gtk.TreeNode(ListOnly = true)]
+	public class SimEntry : Gtk.TreeNode {
+		string simId;
+		string name;
+		string description;
+		string owner;
+
+		public SimEntry() {
+			this.simId = "";
+			this.name = "";
+			this.description = "";
+			this.owner = "";
+		}
+
+		public SimEntry(string line) {
+			string[] parts = line.Split(',');
+			
+			this.simId = parts[1];
+			this.name = parts[3];
+			this.description = parts[4];
+			this.owner = parts[5];
+		}
+
+		[Gtk.TreeNodeValue(Column = 0)]
+		public string SimId {
+			get { return this.simId; }
+		}
+
+		[Gtk.TreeNodeValue(Column = 1)]
+		public string Name {
+			get { return this.name; }
+		}
+
+		[Gtk.TreeNodeValue(Column = 2)]
+		public string Description {
+			get { return this.description; }
+		}
+
+		[Gtk.TreeNodeValue(Column = 3)]
+		public string Owner {
+			get { return this.owner; }
+		}
+		
 	}
 }
