3261e32ddb59fa98bc475cecf139cb16d22c4cdf
Nikola Skoric <nskoric@gmail.com>
2011-10-14 04:47:32 +0200

    Clock.cs variables are retreived in a single TCP call.

--- EGON_cs_API/Clock.cs
+++ EGON_cs_API/Clock.cs
@@ -2,13 +2,20 @@ using System;
 namespace EGON_cs_API {
 	public class Clock {
 		private ErlInterface erlInterface;
+		private bool logTicks;
 
 		public Clock(ErlInterface erlInterface) {
 			this.erlInterface = erlInterface;
+
+			erlInterface.Register(new Connector.Setter(setLogTicks), "{get, es_clock_server, log_ticks}");
+		}
+
+		public void setLogTicks(string val) {
+			this.logTicks = ErlInterface.StringToBool(val);
 		}
 		
 		public bool LogTicks {
-			get { return ErlInterface.StringToBool(this.erlInterface.Call("{get, es_clock_server, log_ticks}")); }
+			get { return this.logTicks; }
 			set { this.erlInterface.Call("{set, es_clock_server, log_ticks, " + ErlInterface.BoolToString(value) + "}"); }
 		}
 		
