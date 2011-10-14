dbda1faa140fcb9f2e06e6b101cda26a2908692f
Nikola Skoric <nskoric@gmail.com>
2011-10-14 04:42:38 +0200

    Turbine.cs variables are retreived in a single TCP call.

--- EGON_cs_API/Turbine.cs
+++ EGON_cs_API/Turbine.cs
@@ -2,32 +2,66 @@ using System;
 namespace EGON_cs_API {
 	public class Turbine {
 		private ErlInterface erlInterface;
+		private float power;
+		private float tref;
+		private float target;
+		private float rate;
+		private bool go;
+
 
 		public Turbine(ErlInterface erlInterface) {
 			this.erlInterface = erlInterface;
+
+			erlInterface.Register(new Connector.Setter(setPower), "{get, es_turbine_server, power}");
+			erlInterface.Register(new Connector.Setter(setTref), "{get, es_turbine_server, tref}");
+			erlInterface.Register(new Connector.Setter(setTarget), "{get, es_turbine_server, target}");
+			erlInterface.Register(new Connector.Setter(setRate), "{get, es_turbine_server, rate}");
+			erlInterface.Register(new Connector.Setter(setGo), "{get, es_turbine_server, go}");
+
 		}
 		
+		public void setPower(string val) {
+			this.power = float.Parse(val.Replace('.', ','));
+		}
+		
+		public void setTref(string val) {
+			this.tref = float.Parse(val.Replace('.', ','));
+		}
+		
+		public void setTarget(string val) {
+			this.target = float.Parse(val.Replace('.', ','));
+		}
+		
+		public void setRate(string val) {
+			this.rate = float.Parse(val.Replace('.', ','));
+		}
+		
+		public void setGo(string val) {
+			this.go = ErlInterface.StringToBool(val);
+		}
+		
+
 		public float Power {
-			get { return float.Parse(this.erlInterface.Call("{get, es_turbine_server, power}")); }
+			get { return this.power; }
 			set { this.erlInterface.Call("{set, es_turbine_server, power, " + value.ToString() + "}\n"); }
 		}
 
 		public float Tref {
-			get { return float.Parse(this.erlInterface.Call("{get, es_w7300_server, tref}")); }
+			get { return this.tref; }
 		}
 
-		public int Target {
-			get { return int.Parse(this.erlInterface.Call("{get, es_turbine_server, target}")); }
+		public float Target {
+			get { return this.target; }
 			set { this.erlInterface.Call("{set, es_turbine_server, target, " + value.ToString() + "}\n"); }
 		}
 
-		public int Rate {
-			get { return int.Parse(this.erlInterface.Call("{get, es_turbine_server, rate}")); }
+		public float Rate {
+			get { return this.rate; }
 			set { this.erlInterface.Call("{set, es_turbine_server, rate, " + value.ToString() + "}\n"); }
 		}
 
 		public bool Go {
-			get { return ErlInterface.StringToBool(this.erlInterface.Call("{get, es_turbine_server, go}")); }
+			get { return this.go; }
 			set {
 				this.erlInterface.Call("{set, es_turbine_server, go, " + ErlInterface.BoolToString(value) + "}");
 			}
